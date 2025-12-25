3fecbe1f93b9942ca4115881201f264bb158e154
MichelleLoveChocobear <juytre0860@gmail.com>
2025-12-26 00:30:28 +0800

    20251226上傳

---
+++ js/Script1.js
@@ -0,0 +1,7 @@
+// JavaScript source code
+function hide() {
+    document.getElementById('introduction').style.display = 'none';
+}
+function show() {
+    document.getElementById('introduction').style.display = 'block';
+}
